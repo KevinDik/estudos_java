4a7538d9a03053807e0b615294974dc9a26f2f92
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-01-19 11:14:52 -0300

    Testando algumas variaveis

---
+++ variaveis/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n1 = 8;
+        int n2 = 5;
+
+        int resultado = n1 + n2;
+        System.out.println("resultado " + resultado);
+    }
+}
