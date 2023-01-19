5c352e6aed260c88c3f34a75bcb6b7d88c67cda3
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-01-19 11:28:18 -0300

    Testado printf com variaveis distintas

--- variaveis/src/App.java
+++ variaveis/src/App.java
@@ -5,5 +5,10 @@ public class App {
 
         int resultado = n1 + n2;
         System.out.println("resultado " + resultado);
+
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
     }
 }
